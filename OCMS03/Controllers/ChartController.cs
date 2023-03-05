987bf8e87bc2cb160c0bb47f78f009c9cbdf57d0
Nlakaniplo Khumalo <nhlakahns@gmail.com>
2023-03-05 15:49:09 +0200

    initial commit

---
+++ OCMS03/Controllers/ChartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OCMS03.Models;
+
+namespace OCMS03.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Helper());
+        }
+     }
+}
